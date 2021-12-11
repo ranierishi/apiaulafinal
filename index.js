a8c944e3a979a3ab75d5edf78d8d55e68ade6875
ranierishi <ranieri.shi@gmail.com>
2021-12-11 00:37:41 -0300

    a

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors')
 const routes = require('./routes/routes');
 
 require('dotenv').config({path: './config/.env'});
-app.use(cors())
+// app.use(cors())
 const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}));
